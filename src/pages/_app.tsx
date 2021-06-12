bbef15514d8c4e3aac30ebff47d5141709d0052c
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-06-12 16:42:39 -0400

    feat: add dark mode & refine home page & add react-icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,35 @@
 import withTwindApp from "@twind/next/app";
+import { tw } from "twind";
 import twindConfig from "../../twind.config";
+import { Nav, NavButton } from "../components/Nav";
+import { IoSunny, IoMoon } from "react-icons/io5";
+import useTheme, { ThemeProvider } from "../hooks/useTheme";
+
+const AppWrapper: React.FC = ({ children }) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <>
+      <Nav>
+        <NavButton onClick={() => toggleTheme()}>
+          {theme === "light" && <IoSunny className={tw`w-8 h-8 text-black`} />}
+          {theme === "dark" && <IoMoon className={tw`w-8 h-8 text-white`} />}
+        </NavButton>
+      </Nav>
+
+      {children}
+    </>
+  );
+};
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </ThemeProvider>
+  );
 }
 
 export default withTwindApp(twindConfig, App);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Project } from "../types/project";
 import ProjectCard from "../components/ProjectCard";
 import { tw } from "twind";
+import useTheme from "../hooks/useTheme";
+import { IoLogoGithub, IoLogoNpm } from "react-icons/io5";
 
 const projects: Project[] = [
   {
@@ -11,15 +13,41 @@ const projects: Project[] = [
 ];
 
 export default function Home() {
+  const { theme } = useTheme();
+
   return (
-    <main className={tw`flex h-screen w-screen justify-center`}>
-      <section className={tw`flex flex-col my-auto text-center`}>
-        <h1 className={tw`text-6xl font-poppins font-bold text-blue-500`}>
-          Jacob Shuman
-        </h1>
-        <h2 className={tw`text-2xl font-italic font-raleway`}>
-          Senior Software Developer
-        </h2>
+    <main
+      className={tw(
+        tw`flex h-screen w-screen justify-center`,
+
+        theme === "light" ? tw`bg-white` : tw`bg-gray-900`
+      )}
+    >
+      <section className={tw`flex flex-col space-y-4 my-auto text-center`}>
+        <div>
+          <h1 className={tw`text-6xl font-poppins font-bold text-blue-500`}>
+            Jacob Shuman
+          </h1>
+
+          <h2
+            className={tw(
+              tw`text-2xl font-italic font-raleway`,
+              theme === "light" ? tw`text-gray-900` : tw`text-gray-300`
+            )}
+          >
+            Senior Software Developer
+          </h2>
+        </div>
+
+        <div className={tw`flex justify-center space-x-4`}>
+          <a href="https://github.com/jacob-shuman/portfolio-v2">
+            <IoLogoGithub className={tw`w-8 h-8`} />
+          </a>
+
+          <a href="https://www.npmjs.com/~jacob.shuman">
+            <IoLogoNpm className={tw`w-8 h-8`} />
+          </a>
+        </div>
       </section>
 
       {/* <section className={tw`flex flex-col`}>
