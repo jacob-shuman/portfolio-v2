0a24980fed42e83f7ff58ad9debe0fe7313025a4
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-06-13 15:33:15 -0400

    feat: reorganize nav in app component hierarchy

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,10 @@
 import { tw } from "twind";
+import useTheme from "../hooks/useTheme";
+import { IoSunnyOutline, IoMoonOutline } from "react-icons/io5";
 
-export interface NavProps {}
+export interface NavContainerProps {}
 
-export const Nav: React.FC<NavProps> = ({ children }) => {
+export const NavContainer: React.FC<NavContainerProps> = ({ children }) => {
   return (
     <div
       className={tw`flex flex-row-reverse p-4 justify-between absolute top-0 left-0 w-full`}
@@ -26,5 +28,24 @@ export const NavButton: React.FC<
     {children}
   </button>
 );
+
+export interface NavProps {}
+
+export const Nav: React.FC<NavProps> = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <NavContainer>
+      <NavButton onClick={() => toggleTheme()}>
+        {theme === "light" && (
+          <IoSunnyOutline className={tw`w-8 h-8 text-black`} />
+        )}
+        {theme === "dark" && (
+          <IoMoonOutline className={tw`w-8 h-8 text-white`} />
+        )}
+      </NavButton>
+    </NavContainer>
+  );
+};
 
 export default Nav;
