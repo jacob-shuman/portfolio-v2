77f0c1203acb9b97be9cb7a6141d27b239fc25bf
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-06-13 15:21:03 -0400

    feat: add dark/light colors to twind config

--- twind.config.ts
+++ twind.config.ts
@@ -2,9 +2,21 @@ import { Configuration } from "twind";
 
 export default {
   theme: {
-    fontFamily: {
-      poppins: ["Poppins"],
-      raleway: ["Raleway"],
+    extend: {
+      colors: {
+        light: {
+          100: "#ffffff",
+          200: "#E2E2E2",
+        },
+        dark: {
+          100: "#303030",
+          200: "#202020",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        raleway: ["Raleway"],
+      },
     },
   },
 } as Configuration;
